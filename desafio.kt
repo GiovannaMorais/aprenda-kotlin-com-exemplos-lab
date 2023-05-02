896b980bfc59cce9d7dffb05562d8bc8b8048765
Giovanna Morais <99766055+GiovannaMorais@users.noreply.github.com>
2023-05-02 10:02:37 -0300

    Feat: add challenge resolution

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,29 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String, var idade: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val nivel: Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+	val newUsuario: Usuario = Usuario("Giovanna", 20)
+    val java:ConteudoEducacional = ConteudoEducacional("Java", 30, Nivel.BASICO)
+    val kotlin:ConteudoEducacional = ConteudoEducacional("Kotlin", 45, Nivel.INTERMEDIARIO)
+    val formaçao: Formacao = Formacao("DIO", listOf(kotlin, java))
+    formaçao.matricular(newUsuario)
+    
+//     println(newUsuario)
+//     println(java)
+//     println(kotlin)
+//     println(formaçao)
+
+    println(formaçao.inscritos)
 }
